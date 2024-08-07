1bb1e8a15d16cda17c4977b884a1ca6fc11f851d
salil-naik <salil.naik27@gmail.com>
2024-08-07 14:59:04 +0000

    build: added build artifacts

--- build/static/chunks/app/about-us/page-aafb2fa0d9c3be0c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[554],{658:function(e,n,s){Promise.resolve().then(s.bind(s,1287)),Promise.resolve().then(s.t.bind(s,8173,23))}},function(e){e.O(0,[159,663,287,971,23,744],function(){return e(e.s=658)}),_N_E=e.O()}]);
